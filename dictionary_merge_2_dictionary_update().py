21019ac9cad3ea06ab38269dbddce6456d24c29d
Robinsonkr <robinsonkrobert@gmail.com>
2021-02-27 12:52:43 +0530

    second commit

--- dictionary_merge_2_dictionary_update().py
+++ dictionary_merge_2_dictionary_update().py
@@ -5,8 +5,15 @@ cars1 = {"audi":20000,"bmw":50000}
 
 cars2 = {"benz": 30000,"toyota":10000}
 
-print(merge_2_dictionary(cars1,cars2))
+merge_2_dictionary(cars1,cars2)
+
 print(cars2)
+
+"""
+
+{'benz': 30000, 'toyota': 10000, 'audi': 20000, 'bmw': 50000}
+
+"""
 
 
 
